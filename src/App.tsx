72f05ae048d9a7e7a8627fd1378cbec82761596b
Kyle Hutchinson <kyle1235@gmail.com>
2024-05-12 22:08:25 +0300

    Questions log answer when clicked

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
 import './App.css';
 import { useState } from 'react';
 import { Question } from './Data';
 
 export default function App({ prop }: any) {
-  const [rollercoaster, setRollercoaster] = useState('Loading...');
-  const [options, setOptions] = useState([] as Array<string>);
+  const [question, setQuestion] = useState({
+    text: "Loading...",
+    options: [] as Array<string>,
+  } as Question);
 
-  const question = prop as Promise<Question>;
+  const pendingQuestion = prop as Promise<Question>;
 
-  question.then(question => {
-    setRollercoaster(question.text);
-    setOptions(question.options)
+  pendingQuestion.then(readyQuestion => {
+    setQuestion(readyQuestion);
+  });
+
+  const optionsUi = question.options.map((option, index) => {
+    const onClick = () => {
+      if (index === question.correctIndex) {
+        console.log('Correct!');
+      } else {
+        console.log('Incorrect!');
+      }
+    };
+
+    return <button key={index} onClick={onClick}>{option}</button>;
   });
 
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          {rollercoaster}
+          {question.text}
         </p>
-        {options.map(option => <button key={option}>{option}</button>)}
+        {optionsUi}
       </header>
     </div>
   );
