330e1bc53dfe3d122c5237c5a6c9f06a9489a008
Kyle Hutchinson <kyle1235@gmail.com>
2024-06-06 22:14:38 +0300

    Updated verison

--- src/ui/Home.tsx
+++ src/ui/Home.tsx
@@ -8,7 +8,7 @@ import { deleteData as deleteDataFromDb, isDataStored as isDataStoredInDb } from
 import Filter from './Filter';
 import { RollercoasterFilter, deleteFilter, filter, saveFilter } from '../logic/FilterRepo';
 
-const APP_VERSION = 'v4.1.0';
+const APP_VERSION = 'v4.1.1';
 
 interface State {
     ui: UiState,
