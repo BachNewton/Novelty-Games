01651ddc81415542d68d7a078d24c2f009aa251b
Kyle Hutchinson <kyle1235@gmail.com>
2024-06-15 00:21:16 +0300

    Updated scrapper to get info from all songs

--- scrapper/scrape.js
+++ scrapper/scrape.js
@@ -1,26 +1,70 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 
+function getMatch(html, regex) {
+    return Array.from(html.matchAll(regex))[0];
+}
+
 (async () => {
-    const URL = 'https://fnzone.es/en/festival';
+    const BASE_URL = 'https://fnzone.es/en/festival';
+    const SONG_URL = 'https://fnzone.es/en/festival/';
 
     const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
 
-    await page.goto(URL);
+    console.log('Opening:', BASE_URL);
+    await page.goto(BASE_URL);
 
     const html = await page.content();
 
-    const songAndArtistRegex = /<strong>(.+?)<\/strong><span>(.+?)<\/span>/g;
-    const songAndArtistMatches = html.matchAll(songAndArtistRegex);
-    const json = Array.from(songAndArtistMatches).map(match => {
-        return {
-            song: match[1],
-            artist: match[2]
-        };
-    });
+    const songLinksRegex = /<a href="\/en\/festival\/(.+?)">/g;
 
-    await fs.promises.writeFile('db/fortniteFestivalSongs.json', JSON.stringify(json));
+    const songNameRegex = /<h1 class="c-ewDgRt c-ewDgRt-ikEhgBV-css">(.+?)<\/h1>/g;
+    const artistRegex = /<strong class="c-ewDgRt c-ewDgRt-KoHnu-variant-tertiary c-ewDgRt-ihgsrmT-css">(.+?)<\/strong>/g;
+    const yearAndLengthRegex = /<strong class="c-ewDgRt c-ewDgRt-bAMqTJ-variant-quaternary c-ewDgRt-iGSDkZ-css">(.+?)<!-- --> · <!-- -->(.+?)<\/strong>/g;
+    const sampleMp3Regex = /<source src="(.+?)" type="audio\/mp3">/g;
+    const albumArtRegex = /<img alt="" loading="lazy" width="180" height="180" decoding="async" data-nimg="1" style="color:transparent" src="(.+?)">/g;
+
+    const songs = [];
+
+    const songLinksMatches = Array.from(html.matchAll(songLinksRegex));
+
+    console.log(`Found ${songLinksMatches.length} links`);
+    songLinksMatches.forEach(match => console.log(SONG_URL + match[1]));
+
+    for (const match of songLinksMatches) {
+        const songLink = match[1];
+        const url = SONG_URL + songLink;
+        console.log('Opening:', url);
+        await page.goto(url);
+        const songHtml = await page.content();
+
+        const songName = getMatch(songHtml, songNameRegex)[1];
+        console.log('Song name:', songName);
+        const artist = getMatch(songHtml, artistRegex)[1];
+        console.log('Artist:', artist);
+        const yearAndLengthMatch = getMatch(songHtml, yearAndLengthRegex);
+        const year = yearAndLengthMatch[1];
+        console.log('Year:', year);
+        const length = yearAndLengthMatch[2];
+        console.log('Length:', length);
+        const sampleMp3Match = getMatch(songHtml, sampleMp3Regex);
+        const sampleMp3 = sampleMp3Match === undefined ? null : sampleMp3Match[1];
+        console.log('Sample MP3:', sampleMp3);
+        const albumArt = getMatch(songHtml, albumArtRegex)[1];
+        console.log('Album Art:', albumArt);
+
+        songs.push({
+            name: songName,
+            artist: artist,
+            year: year,
+            length: length,
+            sampleMp3: sampleMp3,
+            albumArt: albumArt
+        });
+    }
+
+    await fs.promises.writeFile('db/fortniteFestivalSongs.json', JSON.stringify(songs));
 
     await browser.close();
 })();
