dcdd7488fd13579c8f38fdb9f2c8bf53cc7b8d56
Kyle Hutchinson <kyle1235@gmail.com>
2024-06-22 21:53:24 +0300

    Temp add local IP to cors for testing

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:3000'
+        origin: ['http://localhost:3000', 'http://192.168.50.75:3000']
     }
 });
 const PORT = 80;
