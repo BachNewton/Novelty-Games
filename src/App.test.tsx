46369e7aaa8abc9b0953a4b247443acde65448b6
Kyle Hutchinson <kyle1235@gmail.com>
2024-06-25 23:45:49 +0300

    Moved all the trivia code into its own folder

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './ui/Game';
+import App from './trivia/ui/Game';
 
 test('renders learn react link', () => {
   // render(<App temp="" />);
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './css/index.css';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
-import Home from './ui/Home';
+import Home from './trivia/ui/Home';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
