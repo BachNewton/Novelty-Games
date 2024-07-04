e532611fe461f875f60a05fee39a27efe0d898cc
Kyle Hutchinson <kyle1235@gmail.com>
2024-07-05 02:50:15 +0300

    Updated FF scrapper

--- scrapper/scrape.js
+++ scrapper/scrape.js
@@ -1,75 +1,70 @@
 import fs from 'fs';
 import puppeteer from 'puppeteer';
 
+const BASE_URL = 'https://fnzone.es/en/festival';
+const SONG_SELECTOR = '#__next > article.c-lmczj.c-lmczj-icFzQBq-css > div.c-dhzjXW.c-dhzjXW-ikbNnIq-css > a';
+const YEAR_AND_LENGTH_REGEX = /<strong>(\d+?)(?:<!-- -->)? · (?:<!-- -->)?(.+?)<\/strong>/g;
+
 function getMatch(html, regex) {
     return Array.from(html.matchAll(regex))[0];
 }
 
-async function getSong(songHtml) {
-    const songNameRegex = /<h1 class="c-ewDgRt c-ewDgRt-ikEhgBV-css">(.+?)<\/h1>/g;
-    const artistRegex = /<strong class="c-ewDgRt c-ewDgRt-KoHnu-variant-tertiary c-ewDgRt-ihgsrmT-css">(.+?)<\/strong>/g;
-    const yearAndLengthRegex = /<strong class="c-ewDgRt c-ewDgRt-bAMqTJ-variant-quaternary c-ewDgRt-iGSDkZ-css">(.+?)(?:<!-- -->)? · (?:<!-- -->)?(.+?)<\/strong>/g;
-    const sampleMp3Regex = /<source src="(.+?)" type="audio\/mp3">/g;
-    const albumArtSelector = '#__next > div.c-cTzty.c-cTzty-ieGPAZP-css > div.c-cTzty.c-cTzty-ijfAQdd-css img';
-
-    const songName = getMatch(songHtml, songNameRegex)[1];
-    console.log('Song name:', songName);
-    const artist = getMatch(songHtml, artistRegex)[1];
-    console.log('Artist:', artist);
-    const yearAndLengthMatch = getMatch(songHtml, yearAndLengthRegex);
-    const year = yearAndLengthMatch[1];
-    console.log('Year:', year);
-    const length = yearAndLengthMatch[2];
-    console.log('Length:', length);
-    const sampleMp3Match = getMatch(songHtml, sampleMp3Regex);
-    const sampleMp3 = sampleMp3Match === undefined ? null : sampleMp3Match[1];
-    console.log('Sample MP3:', sampleMp3);
-    const albumArt = await page.$eval(albumArtSelector, el => el.getAttribute('src'));
-    console.log('Album Art:', albumArt);
-
-    return {
-        name: songName,
-        artist: artist,
-        year: year,
-        length: length,
-        sampleMp3: sampleMp3,
-        albumArt: albumArt
-    };
+async function getSampleMp3(page) {
+    try {
+        return await page.$eval('source', el => el.src);
+    } catch (error) {
+        return null;
+    }
 }
 
 (async () => {
-    const BASE_URL = 'https://fnzone.es/en/festival';
-    const SONG_URL = 'https://fnzone.es/en/festival/';
-
     const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
 
     console.log('Opening:', BASE_URL);
     await page.goto(BASE_URL);
 
-    const html = await page.content();
+    const songs = await page.$$eval(SONG_SELECTOR, els => els.map(el => {
+        const url = el.href;
+        const name = el.querySelector('strong').textContent;
+        const artist = el.querySelector('span').textContent;
+        const albumArt = el.querySelector('img').src;
 
-    const songLinksRegex = /<a href="\/en\/festival\/(.+?)">/g;
+        return {
+            url: url,
+            name: name,
+            artist: artist,
+            albumArt: albumArt,
+            year: null,
+            length: null,
+            sampleMp3: null
+        };
+    }));
 
-    const songs = [];
+    console.log(`Found ${songs.length} links`);
+    songs.forEach(song => console.log(song.url));
 
-    const songLinksMatches = Array.from(html.matchAll(songLinksRegex));
+    for (const song of songs) {
+        console.log('Opening:', song.url);
+        await page.goto(song.url);
 
-    console.log(`Found ${songLinksMatches.length} links`);
-    songLinksMatches.forEach(match => console.log(SONG_URL + match[1]));
+        const sampleMp3 = await getSampleMp3(page);
 
-    for (const match of songLinksMatches) {
-        const songLink = match[1];
-
-        const url = SONG_URL + songLink;
-        console.log('Opening:', url);
-        await page.goto(url);
         const songHtml = await page.content();
+        const yearAndLengthMatch = getMatch(songHtml, YEAR_AND_LENGTH_REGEX);
+        const year = yearAndLengthMatch[1];
+        const length = yearAndLengthMatch[2];
 
-        const song = await getSong(songHtml);
+        song.year = year;
+        song.length = length;
+        song.sampleMp3 = sampleMp3;
 
-        songs.push(song);
+        delete song.url;
+
+        console.log('Added Song:', song);
     }
+
+    console.log('Writing songs to JSON file');
 
     await fs.promises.writeFile('db/fortniteFestivalSongs.json', JSON.stringify(songs));
 
