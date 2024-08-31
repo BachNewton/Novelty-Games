b03bef23b0285f67bee5289331e82700d06edcd4
Kyle Hutchinson <kyle1235@gmail.com>
2024-08-31 21:44:35 +0300

    Added settings button for Pokemon

--- src/trivia/ui/Home.tsx
+++ src/trivia/ui/Home.tsx
@@ -24,7 +24,7 @@ interface State {
 enum UiState {
     HOME,
     GAME,
-    FILTER
+    SETTINGS
 }
 
 const progressUpdater = new ProgressUpdater();
@@ -80,22 +80,45 @@ const Home: React.FC<HomeProps> = ({ onHomeButtonClicked }) => {
                 onBackClicked={onBackClicked}
                 progressListener={progressUpdater}
             />;
-        case UiState.FILTER:
-            const rollercoasterFilterGetter: RollercoasterFilterGetter = { get: null };
-            const pendingCoasters = state.data as Promise<Array<Rollercoaster>>;
-            const pendingFilterUi = pendingCoasters.then(coasters => {
-                return <Filter
-                    coasters={coasters}
-                    rollercoasterFilterGetter={rollercoasterFilterGetter}
-                />;
-            });
-            return <SettingsPage
-                content={pendingFilterUi}
-                onCancel={onSettingsPageCancelClicked}
-                onConfirm={() => onFilterConfirmClicked(rollercoasterFilterGetter.get?.())}
-            />;
+        case UiState.SETTINGS:
+            return getSettingsUi(state.dataType, state.data, onSettingsPageCancelClicked, onFilterConfirmClicked);
     }
 };
+
+function getSettingsUi(
+    dataType: DataType,
+    data: Promise<Data>,
+    onSettingsPageCancelClicked: () => void,
+    onFilterConfirmClicked: (rollercoasterFilter: RollercoasterFilter | undefined) => void
+): JSX.Element {
+    if (dataType === DataType.ROLLERCOASTERS) {
+        const rollercoasterFilterGetter: RollercoasterFilterGetter = { get: null };
+        const pendingCoasters = data as Promise<Array<Rollercoaster>>;
+        const pendingFilterUi = pendingCoasters.then(coasters => {
+            return <Filter
+                coasters={coasters}
+                rollercoasterFilterGetter={rollercoasterFilterGetter}
+            />;
+        });
+
+        return <SettingsPage
+            content={pendingFilterUi}
+            onCancel={onSettingsPageCancelClicked}
+            onConfirm={() => onFilterConfirmClicked(rollercoasterFilterGetter.get?.())}
+        />;
+    } else if (dataType === DataType.POKEMON) {
+        const content = Promise.resolve(
+            <h1>Pokémon Settings - work in progress</h1>
+        );
+        return <SettingsPage
+            content={content}
+            onCancel={onSettingsPageCancelClicked}
+            onConfirm={() => { }}
+        />;
+    } else {
+        throw new Error('DataType: ' + dataType + ', has no SettingsUi!');
+    }
+}
 
 function HomeUi(
     isDataStored: Map<DataType, boolean>,
@@ -113,7 +136,7 @@ function HomeUi(
     ].map((dataType, index) => {
         const onGameClick = () => {
             const data = getFromRepo(getRepoBaseDataType(dataType), progressUpdater);
-            state.data = hasFilter(dataType) ? filter(data as Promise<Array<Rollercoaster>>) : data;
+            state.data = dataType === DataType.ROLLERCOASTERS ? filter(data as Promise<Array<Rollercoaster>>) : data;
             state.dataType = dataType;
             state.ui = UiState.GAME;
 
@@ -129,14 +152,18 @@ function HomeUi(
             setState({ ...state });
         };
 
-        const onFilterClick = () => {
-            state.data = getFromRepo(DataType.ROLLERCOASTERS, progressUpdater);
-            state.ui = UiState.FILTER;
+        const onSettingsClick = () => {
+            if (dataType === DataType.ROLLERCOASTERS) {
+                state.data = getFromRepo(DataType.ROLLERCOASTERS, progressUpdater);
+            }
+
+            state.dataType = dataType;
+            state.ui = UiState.SETTINGS;
             setState({ ...state });
         };
 
-        const filterButtonUi = hasFilter(dataType) && isDataStored.get(dataType) === true
-            ? <button className='option-button' onClick={onFilterClick}>⚙️</button>
+        const filterButtonUi = hasSettings(dataType) && isDataStored.get(dataType) === true
+            ? <button className='option-button' onClick={onSettingsClick}>⚙️</button>
             : <></>;
 
         const deleteButtonUi = isDataStored.get(dataType) === true || (dataType === DataType.POKEMON && isDataStored.get(DataType.POKEMON_ALL) === true)
@@ -166,7 +193,7 @@ function confirmedDelete(dataType: DataType): boolean {
 
 function deleteData(dataType: DataType, state: State) {
     deleteDataFromDb(dataType);
-    if (hasFilter(dataType)) deleteFilter();
+    if (dataType === DataType.ROLLERCOASTERS) deleteFilter();
 
     state.isDataStored.set(dataType, false);
 }
@@ -206,13 +233,13 @@ function getRepoBaseDataType(dataType: DataType): DataType {
     }
 }
 
-function hasFilter(dataType: DataType): boolean {
+function hasSettings(dataType: DataType): boolean {
     switch (dataType) {
         case DataType.ROLLERCOASTERS:
+        case DataType.POKEMON:
             return true;
         case DataType.MUSIC:
         case DataType.FLAG_GAME:
-        case DataType.POKEMON:
         case DataType.POKEMON_ALL:
         case DataType.FORTNITE_FESTIVAL:
         case DataType.AIRPLANES:
