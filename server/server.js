912265014bea0728175e011a238e87b15226c824
Kyle Hutchinson <kyle1235@gmail.com>
2024-10-01 18:54:51 +0300

    Server auto restarts itself

--- server/server.js
+++ server/server.js
@@ -3,39 +3,53 @@ import { Server } from 'socket.io';
 import { createServer } from 'node:https';
 import fs from 'fs';
 
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/novelty-games.mooo.com/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/novelty-games.mooo.com/fullchain.pem', 'utf8');
-const credentials = { key: privateKey, cert: certificate };
-
-const app = express();
-const server = createServer(credentials, app);
-const io = new Server(server, {
-    cors: {
-        origin: ['https://bachnewton.github.io', 'http://localhost:3000']
-    }
-});
-
 const PORT = 443;
+const PRIVATE_KEY_FILE_PATH = '/etc/letsencrypt/live/novelty-games.mooo.com/privkey.pem';
+const CERTIFICATE_FILE_PATH = '/etc/letsencrypt/live/novelty-games.mooo.com/fullchain.pem';
 
-app.get('/', (_, res) => {
-    res.send('<h1>Novelty Games Server</h1>');
-});
+function startServer() {
+    const privateKey = fs.readFileSync(PRIVATE_KEY_FILE_PATH, 'utf8');
+    const certificate = fs.readFileSync(CERTIFICATE_FILE_PATH, 'utf8');
+    const credentials = { key: privateKey, cert: certificate };
 
-io.on('connection', (socket) => {
-    console.log('Connection:', socket.id);
-    socket.broadcast.emit('connection', socket.id);
-
-    socket.on('disconnect', () => {
-        console.log('Disconnect:', socket.id);
-        socket.broadcast.emit('disconnected', socket.id);
+    const app = express();
+    const server = createServer(credentials, app);
+    const io = new Server(server, {
+        cors: {
+            origin: ['https://bachnewton.github.io', 'http://localhost:3000']
+        }
     });
 
-    socket.on('broadcast', (data) => {
-        console.log('Socket ID:', socket.id, 'Broadcast data:', data);
-        socket.broadcast.emit('broadcast', data);
+    app.get('/', (_, res) => {
+        res.send('<h1>Novelty Games Server</h1>');
     });
-});
 
-server.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
-});
+    io.on('connection', (socket) => {
+        console.log('Connection:', socket.id);
+        socket.broadcast.emit('connection', socket.id);
+
+        socket.on('disconnect', () => {
+            console.log('Disconnect:', socket.id);
+            socket.broadcast.emit('disconnected', socket.id);
+        });
+
+        socket.on('broadcast', (data) => {
+            console.log('Socket ID:', socket.id, 'Broadcast data:', data);
+            socket.broadcast.emit('broadcast', data);
+        });
+    });
+
+    server.listen(PORT, () => {
+        console.log(`Server listening on port ${PORT}`);
+    });
+
+    return server;
+}
+
+(async () => {
+    while (true) {
+        const server = startServer();
+        await new Promise(resolve => setTimeout(resolve, 5000));
+        server.close();
+    }
+})();
