60449404f54c8673afd8e0a5dfc90631e42dc151
Kyle Hutchinson <kyle1235@gmail.com>
2024-10-07 23:47:45 +0300

    First draft of Kyle Bot

--- src/mille-bornes/logic/bots/KyleBot.ts
+++ src/mille-bornes/logic/bots/KyleBot.ts
@@ -1,7 +1,7 @@
 import { removeRandomElement } from "../../../util/Randomizer";
-import { Card, RemedyCard, SafetyCard } from "../Card";
+import { Card, DistanceCard, HazardCard, LimitCard, RemedyCard, SafetyCard, UnlimitedCard } from "../Card";
 import { Team } from "../Data";
-import { getRemainingDistance } from "../Rules";
+import { getRemainingDistance, getTotalDistance } from "../Rules";
 import { Bot } from "./Bot";
 
 interface Choice {
@@ -22,9 +22,11 @@ export const KyleBot: Bot = {
         const unplayableCards = myHand.filter(card => !canCardBePlayed(card));
 
         if (playableCards.length > 0) {
-            //
+            const choice = choosePlayableCard(playableCards, myTeam, otherTeams, canCardBePlayed, gameIsExtended);
+            playCard(choice.card, choice.targetTeam);
         } else {
-            //
+            const choice = chooseUnplayableCards(unplayableCards);
+            playCard(choice.card, choice.targetTeam);
         }
     }
 };
@@ -33,6 +35,7 @@ function choosePlayableCard(
     playableCards: Card[],
     myTeam: Team,
     otherTeams: Team[],
+    canCardBePlayed: (card: Card, targetTeam?: Team | undefined) => boolean,
     gameIsExtended: boolean
 ): Choice {
     const safetyCards = playableCards.filter(card => card instanceof SafetyCard);
@@ -45,9 +48,18 @@ function choosePlayableCard(
         };
     }
 
+    const hazardCards = playableCards.filter(card => card instanceof HazardCard);
+    if (hazardCards.length > 0) {
+        const offensiveChoice = chooseOffensiveCard(otherTeams, hazardCards, canCardBePlayed);
+
+        if (offensiveChoice !== null) {
+            return offensiveChoice;
+        }
+    }
+
     const remedyCards = playableCards.filter(card => card instanceof RemedyCard);
     if (remedyCards.length > 0) {
-        const choosenCard = removeRandomElement(remedyCards);
+        const choosenCard = remedyCards.shift() as RemedyCard;
 
         return {
             card: choosenCard,
@@ -55,7 +67,45 @@ function choosePlayableCard(
         };
     }
 
-    throw new Error("Function not implemented.");
+    const distanceCards = playableCards.filter(card => card instanceof DistanceCard) as DistanceCard[];
+    if (distanceCards.length > 0) {
+        const has200CardBeenPlayed = distanceCards.find(card => card.amount === 200) !== undefined;
+
+        const rankedCards = distanceCards.sort((card1, card2) => {
+            const card1Value = card1.amount === 200 && has200CardBeenPlayed ? card1.amount : 75 - 1;
+            const card2Value = card2.amount === 200 && has200CardBeenPlayed ? card1.amount : 75 - 1;
+
+            return card2Value - card1Value;
+        });
+
+        const choosenCard = rankedCards.shift() as DistanceCard;
+
+        return {
+            card: choosenCard,
+            targetTeam: myTeam
+        };
+    }
+
+    const limitCards = playableCards.filter(card => card instanceof LimitCard);
+    if (limitCards.length > 0) {
+        const offensiveChoice = chooseOffensiveCard(otherTeams, limitCards, canCardBePlayed);
+
+        if (offensiveChoice !== null) {
+            return offensiveChoice;
+        }
+    }
+
+    const unlimitedCards = playableCards.filter(card => card instanceof UnlimitedCard);
+    if (unlimitedCards.length > 0) {
+        const choosenCard = limitCards.shift() as UnlimitedCard;
+
+        return {
+            card: choosenCard,
+            targetTeam: myTeam
+        };
+    }
+
+    throw new Error('Invalid state! Some choice ought to have been made!');
 }
 
 function isAtLeastOneTeamOneCardAwayFromEndingTheRound(
@@ -74,6 +124,40 @@ function isAtLeastOneTeamOneCardAwayFromEndingTheRound(
     }).some(remainingDistance => remainingDistance <= 200);
 }
 
-function chooseDiscardCard(unplayableCards: Card[]) {
-    //
+function sortByFurthestFirst(teams: Team[]): Team[] {
+    return teams.sort((team1, team2) => {
+        return getTotalDistance(team2.tableau.distanceArea) - getTotalDistance(team1.tableau.distanceArea);
+    });
+}
+
+function chooseOffensiveCard(
+    otherTeams: Team[],
+    offensiveCards: HazardCard[] | LimitCard[],
+    canCardBePlayed: (card: Card, targetTeam?: Team | undefined) => boolean
+): Choice | null {
+    const furthestTeamFirst = sortByFurthestFirst(otherTeams);
+
+    while (furthestTeamFirst.length > 0) {
+        const targetTeam = furthestTeamFirst.shift() as Team;
+
+        for (const offensiveCard of offensiveCards) {
+            if (canCardBePlayed(offensiveCard, targetTeam)) {
+                return {
+                    card: offensiveCard,
+                    targetTeam: targetTeam
+                };
+            }
+        }
+    }
+
+    return null;
+}
+
+function chooseUnplayableCards(unplayableCards: Card[]): Choice {
+    const choosenCard = removeRandomElement(unplayableCards);
+
+    return {
+        card: choosenCard,
+        targetTeam: null
+    };
 }
