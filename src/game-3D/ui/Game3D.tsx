8d32e0e505536eb90a08ff360c7bc663ad3a4a8b
Kyle Hutchinson <kyle1235@gmail.com>
2024-10-11 00:31:32 +0300

    Added sailboat

--- src/game-3D/ui/Game3D.tsx
+++ src/game-3D/ui/Game3D.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef } from "react";
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 let hasGameBeenSet = false;
 
@@ -26,16 +27,30 @@ const Game3D: React.FC = () => {
 
         window.addEventListener('resize', resize);
 
+        let sailboat: THREE.Group | null = null;
+
         const geometry = new THREE.BoxGeometry(1, 1, 1);
         const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
         const cube = new THREE.Mesh(geometry, material);
+        cube.translateX(10);
         scene.add(cube);
 
-        camera.position.z = 5;
+        const ambientLight = new THREE.AmbientLight();
+        const directionalLight = new THREE.DirectionalLight(0xffd700);
+        directionalLight.position.set(1, 1, -1);
+        scene.add(ambientLight, directionalLight);
+
+        camera.position.z = 25;
+        camera.position.y = 10;
+
+        const controls = new OrbitControls(camera, renderer.domElement);
+        controls.update();
 
         const animate = () => {
             cube.rotation.x += 0.01;
             cube.rotation.y += 0.01;
+
+            sailboat?.rotateY(0.001);
 
             renderer.render(scene, camera);
         };
@@ -45,8 +60,12 @@ const Game3D: React.FC = () => {
 
         const loader = new GLTFLoader();
 
-        loader.load('/public/models/sailboat/scene.gltf', (gltf) => {
-            scene.add(gltf.scene);
+        loader.load('https://raw.githubusercontent.com/BachNewton/Novelty-Games/refs/heads/game-3D/models/sailboat/scene.gltf', (gltf) => {
+            sailboat = gltf.scene;
+
+            sailboat.scale.multiplyScalar(0.01);
+            sailboat.rotateY(2 * Math.PI / 3);
+            scene.add(sailboat);
         }, (xhr) => {
             console.log((xhr.loaded / xhr.total * 100) + '% loaded');
         }, (error) => {
