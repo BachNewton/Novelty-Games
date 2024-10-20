c82cad96755d2caeaa5dae2ec41ec001e1fc7929
Kyle Hutchinson <kyle1235@gmail.com>
2024-10-20 22:58:51 +0300

    Trying other cats

--- src/game-3D/worlds/sea/Cat.ts
+++ src/game-3D/worlds/sea/Cat.ts
@@ -3,7 +3,7 @@ import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 const CAT_MODEL_URL = 'https://raw.githubusercontent.com/BachNewton/Novelty-Games/refs/heads/game-3D/models/cat/Models/cat.fbx';
-const CAT_MODEL_URL_2 = 'https://raw.githubusercontent.com/BachNewton/Novelty-Games/refs/heads/game-3D/models/cat2/f8rra6i0po0p.gltf';
+const CAT_MODEL_URL_2 = 'https://raw.githubusercontent.com/BachNewton/Novelty-Games/refs/heads/game-3D/models/cat2/cat_rigged';
 
 export default class Cat {
     private object: THREE.Object3D;
@@ -15,7 +15,7 @@ export default class Cat {
         this.object = new THREE.Object3D();
         this.animationMixer = new THREE.AnimationMixer(this.object);
 
-        new FBXLoader().loadAsync(CAT_MODEL_URL).then(ftx => {
+        new FBXLoader().loadAsync(CAT_MODEL_URL_2).then(ftx => {
             const mesh = ftx.children[0] as THREE.SkinnedMesh;
             const oldMaterial = mesh.material as THREE.MeshPhongMaterial;
             const newMaterial = new THREE.MeshStandardMaterial({
@@ -24,23 +24,15 @@ export default class Cat {
             });
             mesh.material = newMaterial;
 
-            // const helper = new THREE.SkeletonHelper(ftx);
-            // scene.add(helper);
+            const helper = new THREE.SkeletonHelper(ftx);
+            scene.add(helper);
 
             this.animationMixer = new THREE.AnimationMixer(ftx);
             this.animationActions = ftx.animations.map(animation => this.animationMixer.clipAction(animation));
 
             ftx.scale.multiplyScalar(0.02);
 
-            // this.object.add(ftx);
-        }).catch(error => {
-            console.error(error);
-        });
-
-        new GLTFLoader().loadAsync(CAT_MODEL_URL_2).then(gltf => {
-            const loadedCat = gltf.scene.children[0];
-
-            scene.add(loadedCat);
+            this.object.add(ftx);
         }).catch(error => {
             console.error(error);
         });
