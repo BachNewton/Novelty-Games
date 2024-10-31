a8a3d6ebfbf6dbe656937b8d687552b4154e5314
Kyle Hutchinson <kyle1235@gmail.com>
2024-10-31 16:24:41 +0200

    Created a MarbleWorld

---
+++ src/game-3D/worlds/marble/MarbleWorld.ts
@@ -0,0 +1,9 @@
+import { GameWorld } from "../GameWorld";
+
+const MarbleWorld: GameWorld = {
+    update: (deltaTime) => {
+        throw new Error("Function not implemented.");
+    }
+};
+
+export default MarbleWorld;
