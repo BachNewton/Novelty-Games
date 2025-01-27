ca5e99ec9d45cc35de227ed92d9a08aeae72b6cc
Kyle Hutchinson <kyle1235@gmail.com>
2025-01-27 18:18:29 +0200

    Piece UI is working

--- src/board-games/labyrinth/ui/Labyrinth.tsx
+++ src/board-games/labyrinth/ui/Labyrinth.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { Route, updateRoute } from "../../../ui/Routing";
+import { getRandomElement } from "../../../util/Randomizer";
+import BrickImage from '../images/brick.avif';
 
 interface Piece {
     paths: number[];
@@ -28,60 +30,87 @@ const Labyrinth: React.FC = () => {
         updateRoute(Route.LABYRINTH);
     }, []);
 
-    const piecesUi = pieces.flatMap(rowPieces => rowPieces.map(piece => {
-        return pieceUi(piece);
-    }));
+    const piecesUi = pieces.flatMap(rowPieces => rowPieces).map((piece, index) => {
+        return pieceUi(piece, index);
+    });
 
-    console.log(piecesUi);
-
-    return <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gridAutoRows: 'repeat(3, 1fr)', height: '100vh' }}>
-        {piecesUi}
+    return <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        <div style={{ display: 'grid', gridTemplateColumns: 'repeat(7, 1fr)', gridTemplateRows: 'repeat(7, 1fr)', aspectRatio: 1, maxHeight: '100vh' }}>
+            {piecesUi}
+        </div>
     </div>;
 };
 
-function pieceUi(piece: Piece): JSX.Element {
+function pieceUi(piece: Piece, key: number): JSX.Element {
     const coordinates = piece.paths.map(path => {
         const x = Math.cos(path + piece.rotation);
         const y = Math.sin(path + piece.rotation);
 
-        return { x: Math.floor(x), y: Math.floor(y) };
+        return { x: Math.round(x), y: Math.round(y) };
     });
 
     const top = coordinates.find(coordinate => coordinate.x === 0 && coordinate.y === 1) === undefined
-        ? <div style={{ background: 'blue' }}></div>
+        ? brickUi()
         : <div></div>;
 
     const bottom = coordinates.find(coordinate => coordinate.x === 0 && coordinate.y === -1) === undefined
-        ? <div style={{ background: 'magenta' }}></div>
+        ? brickUi()
         : <div></div>;
 
     const left = coordinates.find(coordinate => coordinate.x === -1 && coordinate.y === 0) === undefined
-        ? <div style={{ background: 'green' }}></div>
+        ? brickUi()
         : <div></div>;
 
     const right = coordinates.find(coordinate => coordinate.x === 1 && coordinate.y === 0) === undefined
-        ? <div style={{ background: 'orange' }}></div>
+        ? brickUi()
         : <div></div>;
 
-    return <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gridAutoRows: 'repeat(3, 1fr)', margin: '5px', border: '2px solid white' }}>
-        <div style={{ background: 'white' }}></div>
+    return <div key={key} style={{
+        display: 'grid',
+        gridTemplateColumns: 'repeat(3, minmax(0, 1fr))',
+        gridTemplateRows: 'repeat(3, 1fr)',
+        margin: '1px',
+        border: '1px solid grey',
+        borderRadius: '15%',
+        overflow: 'hidden',
+        placeItems: 'stretch'
+    }}>
+        {brickUi()}
         {top}
-        <div style={{ background: 'red' }}></div>
+        {brickUi()}
         {left}
         <div></div>
         {right}
-        <div style={{ background: 'black' }}></div>
+        {brickUi()}
         {bottom}
-        <div style={{ background: 'teal' }}></div>
+        {brickUi()}
     </div>;
 }
 
 function startingPieces(): Piece[][] {
     return [
-        [{ ...CORNER_PIECE }, { ...T_PIECE }, { ...T_PIECE }],
-        [{ ...T_PIECE }, { ...T_PIECE }, { ...T_PIECE }],
-        [{ ...T_PIECE }, { ...STRAIGHT_PIECE }, { ...T_PIECE }]
+        [{ ...CORNER_PIECE }, randomPiece(), { ...T_PIECE }, randomPiece(), { ...T_PIECE }, randomPiece(), { ...CORNER_PIECE }],
+        [randomPiece(), randomPiece(), randomPiece(), randomPiece(), randomPiece(), randomPiece(), randomPiece()],
+        [{ ...T_PIECE }, randomPiece(), { ...T_PIECE }, randomPiece(), { ...T_PIECE }, randomPiece(), { ...T_PIECE }],
+        [randomPiece(), randomPiece(), randomPiece(), randomPiece(), randomPiece(), randomPiece(), randomPiece()],
+        [{ ...T_PIECE }, randomPiece(), { ...T_PIECE }, randomPiece(), { ...T_PIECE }, randomPiece(), { ...T_PIECE }],
+        [randomPiece(), randomPiece(), randomPiece(), randomPiece(), randomPiece(), randomPiece(), randomPiece()],
+        [{ ...CORNER_PIECE }, randomPiece(), { ...T_PIECE }, randomPiece(), { ...T_PIECE }, randomPiece(), { ...CORNER_PIECE }]
     ];
+}
+
+function randomPiece(): Piece {
+    const pieceTypes = [{ ...CORNER_PIECE }, { ...T_PIECE }, { ...STRAIGHT_PIECE }];
+    const rotations = [0, Math.PI / 2, Math.PI, -Math.PI / 2];
+
+    const piece = getRandomElement(pieceTypes);
+    piece.rotation += getRandomElement(rotations);
+
+    return piece;
+}
+
+function brickUi(): JSX.Element {
+    return <img src={BrickImage} />
 }
 
 export default Labyrinth;
