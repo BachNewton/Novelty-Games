cb7678d9f43097d9b886dc0ec1546fc48615771e
Kyle Hutchinson <kyle1235@gmail.com>
2025-02-08 23:19:24 +0200

    Demo many different cat animations

--- src/game-2D/worlds/cat/CatWorld.ts
+++ src/game-2D/worlds/cat/CatWorld.ts
@@ -11,26 +11,63 @@ export function createCatWorld(canvas: HTMLCanvasElement, ctx: CanvasRenderingCo
 
     const frameWidth = 32; // Width of a single frame
     const frameHeight = 32; // Height of a single frame
-    const framesPerSecond = 2.5; // Animation speed
+    const framesPerSecond = 4; // Animation speed
 
-    const animator = createAnimator(ctx, image, frameWidth, frameHeight, framesPerSecond);
-    animator.setDebug(true);
+    const animations: Animation[] = [
+        { startingWindowX: 0, startingWindowY: 0, totalWindowsX: 8, totalWindowsY: 1 },
+        { startingWindowX: 0, startingWindowY: 1, totalWindowsX: 8, totalWindowsY: 1 },
+        { startingWindowX: 0, startingWindowY: 2, totalWindowsX: 8, totalWindowsY: 1 },
+        { startingWindowX: 0, startingWindowY: 3, totalWindowsX: 8, totalWindowsY: 1 },
+        { startingWindowX: 0, startingWindowY: 4, totalWindowsX: 8, totalWindowsY: 1 },
+        { startingWindowX: 0, startingWindowY: 5, totalWindowsX: 8, totalWindowsY: 1 },
+        { startingWindowX: 0, startingWindowY: 6, totalWindowsX: 4, totalWindowsY: 1 },
+        { startingWindowX: 0, startingWindowY: 7, totalWindowsX: 2, totalWindowsY: 1 },
+        { startingWindowX: 0, startingWindowY: 8, totalWindowsX: 3, totalWindowsY: 1 },
+        { startingWindowX: 0, startingWindowY: 9, totalWindowsX: 4, totalWindowsY: 1 },
+        { startingWindowX: 0, startingWindowY: 10, totalWindowsX: 8, totalWindowsY: 1 },
+        { startingWindowX: 0, startingWindowY: 11, totalWindowsX: 8, totalWindowsY: 1 },
+        { startingWindowX: 0, startingWindowY: 12, totalWindowsX: 4, totalWindowsY: 1 },
+        { startingWindowX: 0, startingWindowY: 13, totalWindowsX: 4, totalWindowsY: 1 },
+        { startingWindowX: 0, startingWindowY: 14, totalWindowsX: 4, totalWindowsY: 1 },
+        { startingWindowX: 0, startingWindowY: 15, totalWindowsX: 4, totalWindowsY: 1 },
+        { startingWindowX: 0, startingWindowY: 16, totalWindowsX: 4, totalWindowsY: 1 },
+        { startingWindowX: 0, startingWindowY: 17, totalWindowsX: 8, totalWindowsY: 1 },
+        { startingWindowX: 0, startingWindowY: 18, totalWindowsX: 8, totalWindowsY: 1 },
+        { startingWindowX: 0, startingWindowY: 19, totalWindowsX: 4, totalWindowsY: 2 },
+        { startingWindowX: 0, startingWindowY: 21, totalWindowsX: 4, totalWindowsY: 1 },
+        { startingWindowX: 0, startingWindowY: 22, totalWindowsX: 4, totalWindowsY: 1 },
+        { startingWindowX: 0, startingWindowY: 23, totalWindowsX: 4, totalWindowsY: 1 },
+        { startingWindowX: 0, startingWindowY: 24, totalWindowsX: 4, totalWindowsY: 1 },
+        { startingWindowX: 0, startingWindowY: 25, totalWindowsX: 4, totalWindowsY: 1 },
+        { startingWindowX: 0, startingWindowY: 26, totalWindowsX: 4, totalWindowsY: 1 },
+        { startingWindowX: 0, startingWindowY: 27, totalWindowsX: 4, totalWindowsY: 1 },
+        { startingWindowX: 0, startingWindowY: 28, totalWindowsX: 4, totalWindowsY: 1 },
+        { startingWindowX: 0, startingWindowY: 29, totalWindowsX: 4, totalWindowsY: 1 },
+        { startingWindowX: 0, startingWindowY: 30, totalWindowsX: 4, totalWindowsY: 1 },
+        { startingWindowX: 0, startingWindowY: 31, totalWindowsX: 4, totalWindowsY: 1 },
+        { startingWindowX: 0, startingWindowY: 32, totalWindowsX: 4, totalWindowsY: 1 }
+    ];
 
-    const animation: Animation = {
-        startingWindowX: 0,
-        startingWindowY: 0,
-        totalWindowsX: 8,
-        totalWindowsY: 1
-    };
+    const animators = Array.from({ length: animations.length }, () => createAnimator(ctx, image, frameWidth, frameHeight, framesPerSecond));
 
-    animator.setAnimation(animation);
+    animators.forEach((animator, index) => animator.setAnimation(animations[index]));
+
+    // animators[animations.length - 1].setAnimation(animations[animations.length - 1]);
+    // animators[animations.length - 1].setDebug(true);
 
     return {
         draw: () => {
-            animator.draw(750, 250, 120, 120);
+            const cols = 4;
+            const size = canvas.width / cols;
+
+            animators.forEach((animator, index) => {
+                const width = size * (index % cols);
+                const height = size * Math.floor(index / cols);
+                animator.draw(width, height, size, size);
+            });
         },
         update: (deltaTime: number) => {
-            animator.update(deltaTime);
+            animators.forEach(animator => animator.update(deltaTime));
         },
         onTouchStart: (e: TouchEvent) => {
             // console.log("Touch Start in Cat World");
