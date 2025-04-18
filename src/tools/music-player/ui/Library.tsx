4bba1e9142caab19f5bea2ae7932c6d397a5c2cf
Kyle Hutchinson <kyle1235@gmail.com>
2025-04-18 19:13:49 +0300

    Library UI now is sorted and has artist sections

--- src/tools/music-player/ui/Library.tsx
+++ src/tools/music-player/ui/Library.tsx
@@ -8,29 +8,77 @@ interface LibraryProps {
 }
 
 const Library: React.FC<LibraryProps> = ({ songs, onSongSelected }) => {
-    return <div style={{ padding: '10px', display: 'flex', flexDirection: 'column', gap: '5px' }}>
+    return <div>
         {contentUi(songs, onSongSelected)}
     </div>;
 };
 
 function contentUi(songs: ParsedSongPackage[] | null, onSongSelected: (song: SongPackage) => void): JSX.Element | JSX.Element[] {
     if (songs === null) {
-        return <Loading />;
+        return <div style={{ marginTop: '25px' }}>
+            <Loading />
+        </div>;
     } else if (songs.length === 0) {
         return <div>Your music library is empty</div>;
     } else {
-        return songs.sort((a, b) => a.metadata.artist.localeCompare(b.metadata.artist)).map((song, index) => <div
-            key={index}
-            style={{
-                fontSize: '0.8em',
-                padding: '5px',
-                border: '2px solid var(--novelty-blue)',
-                borderRadius: '10px',
-                cursor: 'pointer'
-            }}
-            onClick={() => onSongSelected(song)}
-        >{song.metadata.artist} - {song.metadata.title}</div>);
+        return songsUi(songs, onSongSelected);
     }
+}
+
+function songsUi(songs: ParsedSongPackage[], onSongSelected: (song: SongPackage) => void): JSX.Element[] {
+    const groupedSongs = groupSongsByArtist(songs);
+
+    return Object.entries(groupedSongs)
+        .sort((a, b) => a[0].localeCompare(b[0]))
+        .map(([artist, songs]) => artistSectionUi(artist, songs, onSongSelected));
+}
+
+function groupSongsByArtist(songs: ParsedSongPackage[]): { [artist: string]: ParsedSongPackage[] } {
+    return songs.reduce((acc, song) => {
+        const artist = song.metadata.artist;
+
+        if (!acc[artist]) {
+            acc[artist] = [];
+        }
+
+        acc[artist].push(song);
+
+        return acc;
+    }, {} as { [artist: string]: ParsedSongPackage[] });
+}
+
+function artistSectionUi(artist: string, songs: ParsedSongPackage[], onSongSelected: (song: SongPackage) => void): JSX.Element {
+    return <div key={artist} style={{}}>
+        <div
+            style={{
+                position: 'sticky',
+                top: 0,
+                backgroundColor: 'var(--novelty-blue)',
+                padding: '5px',
+                fontWeight: 'bold'
+            }}
+        >
+            {artist}
+        </div>
+        <div>
+            {songs.map((song, index) => songUi(index, song, onSongSelected))}
+        </div>
+    </div>;
+}
+
+function songUi(index: number, song: ParsedSongPackage, onSongSelected: (song: SongPackage) => void): JSX.Element {
+    return <div
+        key={index}
+        style={{
+            fontSize: '0.8em',
+            padding: '5px',
+            margin: '5px',
+            border: '2px solid var(--novelty-blue)',
+            borderRadius: '10px',
+            cursor: 'pointer'
+        }}
+        onClick={() => onSongSelected(song)}
+    >{song.metadata.title}</div>;
 }
 
 export default Library;
