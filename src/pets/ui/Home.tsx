b5577a15fb9f06de89c6259f3976f0c02b2fd43f
Kyle Hutchinson <kyle1235@gmail.com>
2025-07-18 16:47:36 +0300

    Basic layout

--- src/pets/ui/Home.tsx
+++ src/pets/ui/Home.tsx
@@ -1,5 +1,8 @@
 import { useEffect } from "react";
 import { Route, updateRoute } from "../../ui/Routing";
+import Scaffold from "../../util/ui/Scaffold";
+import Button from "../../util/ui/Button";
+import DemoImage from "../images/demo.jpeg";
 
 interface HomeProps { }
 
@@ -8,7 +11,29 @@ const Home: React.FC<HomeProps> = ({ }) => {
         updateRoute(Route.PETS);
     }, []);
 
-    return <div>Work in progress</div>;
+    return <Scaffold
+        header={headerUi()}
+        footer={footerUi()}
+        fontScale={1.5}
+    >
+        <div style={{ display: 'flex', height: '100%', alignItems: 'center', justifyContent: 'center' }}>
+            <img src={DemoImage} alt='' style={{ maxWidth: '100%', maxHeight: '100%' }} />
+        </div>
+    </Scaffold>;
 };
+
+function headerUi(): JSX.Element {
+    return <div style={{ borderBottom: '5px solid blue' }}>
+        <div>Header 1</div><div>Header 2</div><div>Header 3</div>
+    </div>;
+}
+
+function footerUi(): JSX.Element {
+    return <div style={{ display: 'flex', borderTop: '5px solid blue' }}>
+        <Button>Greet</Button>
+        <Button>Pet</Button>
+        <Button>Feed</Button>
+    </div>;
+}
 
 export default Home;
