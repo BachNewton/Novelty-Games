dada93bd174590ab4f5ec5e9f125a518d19e00fb
Kyle Hutchinson <kyle1235@gmail.com>
2025-07-18 13:22:57 +0300

    Fixed center of mouse click

--- src/game-2D/worlds/platformer/PlatformerWorld.ts
+++ src/game-2D/worlds/platformer/PlatformerWorld.ts
@@ -48,7 +48,7 @@ export function createPlatformerWorld(drawer: Drawer, camera: Camera, keyboardIn
         },
         mouseEvents: {
             onMouseDown: () => {
-                obstacles.push({ position: createVector(camera.mousePosition.x - 10, camera.mousePosition.y - 10), width: 20, height: 20, });
+                obstacles.push({ position: createVector(camera.mousePosition.x, camera.mousePosition.y), width: 20, height: 20, });
             }
         }
     };
