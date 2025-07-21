4fe22837e2b66ad5bab2df6941ffb3cc9b034266
Kyle Hutchinson <kyle1235@gmail.com>
2025-07-21 14:01:52 +0300

    Fixed mistake. Hidden and greeting were flipped for default dialouge

--- src/pets/data/Dialogue.ts
+++ src/pets/data/Dialogue.ts
@@ -6,8 +6,8 @@ export interface Dialogue {
 
 export function getDefaultDialogue(): Dialogue {
     return {
-        hidden: 'Hello, I am a pet. I am awake and this is my dialogue. This game is a work in progress. In the future I will say some really cute things. Right now you can greet me, pet me, or feed me. But these are just some placeholder options and they don\'t do anything.',
-        greeting: 'I am a pet and I am hidden. Come find me!',
+        hidden: 'I am a pet and I am hidden. Come find me!',
+        greeting: 'Hello, I am a pet. I am awake and this is my dialogue. This game is a work in progress. In the future I will say some really cute things. Right now you can greet me, pet me, or feed me. But these are just some placeholder options and they don\'t do anything.',
         sleeping: 'I am a sleeping pet. I don\'t want to interact right now. How about you come back later when I am awake?'
     };
 }
