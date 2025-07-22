218e80da4aad04b4c60508e35e900cb47581ffcd
Kyle Hutchinson <kyle1235@gmail.com>
2025-07-23 00:25:10 +0300

    Updated default greet dialouge

--- src/pets/data/Dialogue.ts
+++ src/pets/data/Dialogue.ts
@@ -7,7 +7,7 @@ export interface Dialogue {
 export function getDefaultDialogue(): Dialogue {
     return {
         hidden: 'I am a pet and I am hidden. Come find me!',
-        greeting: 'Hello, I am a pet. I am awake and this is my dialogue. This game is a work in progress. In the future I will say some really cute things. Right now you can greet me, pet me, or feed me. But these are just some placeholder options and they don\'t do anything.',
+        greeting: 'Hello, I am a pet. I am awake and this is my dialogue. This game is a work in progress. In the future I will say some really cute things. How about you try some of my interactions below?',
         sleeping: 'I am a sleeping pet. I don\'t want to interact right now. How about you come back later when I am awake?'
     };
 }
