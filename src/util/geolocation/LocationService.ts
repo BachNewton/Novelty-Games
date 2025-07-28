346d26f8153c148938ec835faa9c19ae79b06c0d
Kyle Hutchinson <kyle1235@gmail.com>
2025-07-28 15:22:31 +0300

    LocationService can now watch location

--- src/util/geolocation/LocationService.ts
+++ src/util/geolocation/LocationService.ts
@@ -1,5 +1,7 @@
 export interface LocationService {
     getLocation: () => Promise<Location>;
+    watchLocation: (onLocationUpdate: (location: Location) => void) => void;
+    stopWatching: () => void;
 }
 
 export interface Location {
@@ -8,20 +10,37 @@ export interface Location {
 }
 
 export function createLocationService(): LocationService {
+    let watchId: number | null = null;
+
     return {
         getLocation: () => new Promise((resolve, reject) => {
             navigator.geolocation.getCurrentPosition(position => {
-                const location: Location = {
-                    lat: position.coords.latitude,
-                    lon: position.coords.longitude
-                };
-
-                resolve(location);
+                resolve(toLocation(position));
             }, error => {
                 reject(error);
             }, {
                 enableHighAccuracy: true
             });
-        })
+        }),
+        watchLocation: (onLocationUpdate) => {
+            watchId = navigator.geolocation.watchPosition(position => {
+                onLocationUpdate(toLocation(position));
+            }, _ => {
+                // Ignore errors
+            }, {
+                enableHighAccuracy: true
+            });
+        },
+        stopWatching: () => {
+            if (watchId === null) return;
+            navigator.geolocation.clearWatch(watchId);
+        }
     }
 };
+
+function toLocation(position: GeolocationPosition): Location {
+    return {
+        lat: position.coords.latitude,
+        lon: position.coords.longitude
+    };
+}
