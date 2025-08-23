02408013aac7ff30ea1f88d3ee9b5915add82eef
Kyle Hutchinson <kyle1235@gmail.com>
2025-08-23 14:58:06 +0300

    Created a Tile object

--- src/game-2D/worlds/rpg/RpgWorld.ts
+++ src/game-2D/worlds/rpg/RpgWorld.ts
@@ -3,11 +3,9 @@ import { KeyboardInput } from "../../../util/input/Keyboard";
 import { Camera } from "../Camera";
 import { Drawer } from "../Drawer";
 import { GameWorld } from "../GameWorld";
-import { Box } from "../Geometry";
-import { createVector } from "../Vector";
+import { createTile, Tile, TILE_SIZE } from "./data/Tile";
 import { getOverlay } from "./ui/Main";
 
-const TILE_SIZE = 35;
 const CAMERA_SPEED = 0.25;
 
 export function createRpgWorld(
@@ -19,45 +17,29 @@ export function createRpgWorld(
 ): GameWorld {
     updateRoute(Route.RPG);
 
-    const selectedCell: Box = {
-        position: createVector(0, 0),
-        width: TILE_SIZE,
-        height: TILE_SIZE,
-        borderColor: 'yellow',
-        borderWidth: 2
-    };
-
-    const centerBox: Box = {
-        position: createVector(0, 0),
-        width: TILE_SIZE,
-        height: TILE_SIZE,
-        borderColor: 'white',
-        borderWidth: 1
-    };
+    const selectedTile = createTile(drawer, true);
+    const centerTile = createTile(drawer, false);
 
     return {
         draw: () => {
-            drawer.draw(centerBox);
-            drawer.draw(selectedCell);
+            centerTile.draw();
+            selectedTile.draw();
         },
 
         update: (deltaTime) => {
             camera.position.add(keyboardInput.movementAxis, deltaTime * CAMERA_SPEED);
 
-            const tileX = Math.floor(camera.mousePosition.x / TILE_SIZE);
-            const tileY = Math.floor(camera.mousePosition.y / TILE_SIZE);
-
-            const centerX = (tileX * TILE_SIZE) + (TILE_SIZE / 2);
-            const centerY = (tileY * TILE_SIZE) + (TILE_SIZE / 2);
-
-            selectedCell.position.set(centerX, centerY);
-
-            // console.log(camera.position.x, camera.position.y);
-            // console.log(keyboardInput.movementAxis);
-            // console.log(camera.mousePosition.x, camera.mousePosition.y);
-            // console.log(tileX, tileY);
+            updateSelectedTile(camera, selectedTile);
         },
 
         overlay: getOverlay()
     };
+}
+
+function updateSelectedTile(camera: Camera, selectedTile: Tile) {
+    const tileX = Math.floor(camera.mousePosition.x / TILE_SIZE);
+    const tileY = Math.floor(camera.mousePosition.y / TILE_SIZE);
+
+    selectedTile.x = tileX;
+    selectedTile.y = tileY;
 }
---
+++ src/game-2D/worlds/rpg/data/Tile.ts
@@ -0,0 +1,42 @@
+import { Drawer } from "../../Drawer";
+import { GameObject } from "../../GameWorld";
+import { Box } from "../../Geometry";
+import { createVector } from "../../Vector";
+
+export const TILE_SIZE = 35;
+
+export interface Tile extends GameObject {
+    x: number;
+    y: number;
+}
+
+export function createTile(drawer: Drawer, isHighlight: boolean): Tile {
+    let x = 0;
+    let y = 0;
+
+    const box: Box = {
+        position: createVector(0, 0),
+        width: TILE_SIZE,
+        height: TILE_SIZE,
+        borderColor: isHighlight ? 'yellow' : 'white',
+        borderWidth: isHighlight ? 2 : 1
+    };
+
+    return {
+        get x() { return x; },
+        set x(value: number) { x = value; },
+        get y() { return y; },
+        set y(value: number) { y = value; },
+
+        draw: () => {
+            const centerX = (x * TILE_SIZE) + (TILE_SIZE / 2);
+            const centerY = (y * TILE_SIZE) + (TILE_SIZE / 2);
+
+            box.position.set(centerX, centerY);
+
+            drawer.draw(box);
+        },
+
+        update: (deltaTime) => { }
+    };
+}
