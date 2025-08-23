1b777010fd9735c25f65579aa63885d50c0c7f41
Kyle Hutchinson <kyle1235@gmail.com>
2025-08-23 13:17:19 +0300

    Simple side pannel

--- src/game-2D/worlds/rpg/ui/Main.tsx
+++ src/game-2D/worlds/rpg/ui/Main.tsx
@@ -1,7 +1,19 @@
 interface MainProps { }
 
 const Main: React.FC<MainProps> = ({ }) => {
-    return <div>This is an RPG</div>;
+    return <div style={{
+        display: 'flex',
+        alignItems: 'center',
+        height: '100%'
+    }}>
+        <div style={{
+            border: '2px white solid',
+            borderRadius: '15px',
+            height: '75%',
+            margin: '10px',
+            padding: '10px'
+        }}>Edit Pannel</div>
+    </div>;
 };
 
 export function getOverlay(): JSX.Element {
