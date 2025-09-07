8fe10e6b3e27a41ff24a8ed7128dc33399b9d186
Kyle Hutchinson <kyle1235@gmail.com>
2025-09-08 01:57:28 +0300

    Added Go square

--- src/board-games/monopoly/ui/Square.tsx
+++ src/board-games/monopoly/ui/Square.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Side, Square as SquareData } from "../data/Square";
+import GoIcon from '../icon/go.png';
 
 const STREET_COLOR_WIDTH = 12;
 
@@ -9,21 +10,24 @@ interface SquareProps {
 
 const Square: React.FC<SquareProps> = ({ data }) => {
     return <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
         width: '100%',
         height: '100%',
         boxSizing: 'border-box',
         ...getBorder(data)
     }}>
-        {/* {data.name} */}
+        {getContent(data)}
     </div>;
 };
 
-function getBorder(street: SquareData): React.CSSProperties {
-    if (street.type !== 'street') return {};
+function getBorder(data: SquareData): React.CSSProperties {
+    if (data.type !== 'street') return {};
 
-    const border = `${STREET_COLOR_WIDTH}px solid ${street.color}`;
+    const border = `${STREET_COLOR_WIDTH}px solid ${data.color}`;
 
-    switch (street.side) {
+    switch (data.side) {
         case Side.BOTTOM:
             return { borderTop: border };
         case Side.LEFT:
@@ -32,6 +36,13 @@ function getBorder(street: SquareData): React.CSSProperties {
             return { borderBottom: border };
         case Side.RIGHT:
             return { borderLeft: border };
+    }
+}
+
+function getContent(data: SquareData): React.ReactNode {
+    switch (data.type) {
+        case 'go':
+            return <img src={GoIcon} alt="Go" style={{ width: '75%', height: '75%' }} />;
     }
 }
 
