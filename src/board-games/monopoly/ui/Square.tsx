256491ffc24f8ab1eabd2e3a2b0484c1f99f700e
Kyle Hutchinson <kyle1235@gmail.com>
2025-09-08 01:11:30 +0300

    Added border colors

--- src/board-games/monopoly/ui/Square.tsx
+++ src/board-games/monopoly/ui/Square.tsx
@@ -1,4 +1,7 @@
-import { Square as SquareData } from "../data/Square";
+import React from "react";
+import { Side, Square as SquareData } from "../data/Square";
+
+const STREET_COLOR_WIDTH = 12;
 
 interface SquareProps {
     data: SquareData;
@@ -8,10 +11,28 @@ const Square: React.FC<SquareProps> = ({ data }) => {
     return <div style={{
         width: '100%',
         height: '100%',
-        backgroundColor: data.type === 'street' ? data.color : undefined
+        boxSizing: 'border-box',
+        ...getBorder(data)
     }}>
         {data.name}
     </div>;
 };
+
+function getBorder(street: SquareData): React.CSSProperties {
+    if (street.type !== 'street') return {};
+
+    const border = `${STREET_COLOR_WIDTH}px solid ${street.color}`;
+
+    switch (street.side) {
+        case Side.BOTTOM:
+            return { borderTop: border };
+        case Side.LEFT:
+            return { borderRight: border };
+        case Side.TOP:
+            return { borderBottom: border };
+        case Side.RIGHT:
+            return { borderLeft: border };
+    }
+}
 
 export default Square;
