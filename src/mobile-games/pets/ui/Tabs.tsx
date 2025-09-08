c266596c5fec0986ee6940fc965b9f6952cb52d0
Kyle Hutchinson <kyle1235@gmail.com>
2025-09-08 11:14:39 -0400

    Added a menu options to the tabs

--- src/mobile-games/pets/ui/Tabs.tsx
+++ src/mobile-games/pets/ui/Tabs.tsx
@@ -8,32 +8,33 @@ interface TabsProps {
 }
 
 const Tabs: React.FC<TabsProps> = ({ pets, selectedTab, onTabSelected }) => {
-    const tabs = [
-        // <div
-        //     key='menu'
-        //     style={getTabStyle(selectedTab, 0)}
-        //     onClick={() => onTabSelected(null)}
-        // >
-        //     Menu
-        // </div>,
-        ...pets.map((pet, index) => {
-            const name = pet.discovered ? pet.name : '???';
-            return <div
-                key={index}
-                style={getTabStyle(selectedTab, index)}
-                onClick={() => onTabSelected(index)}
-            >
-                {name}
-            </div>;
-        })
-    ];
+    const tabs = pets.map((pet, index) => {
+        const name = pet.discovered ? pet.name : '???';
 
-    return <div style={{ display: 'flex', overflow: 'auto', backgroundColor: COLORS.surface }}>
-        {tabs}
+        return <div
+            key={index}
+            style={getTabStyle(selectedTab, index)}
+            onClick={() => onTabSelected(index)}
+        >
+            {name}
+        </div>;
+    });
+
+    return <div style={{ display: 'flex' }}>
+        {menuUi(selectedTab)}
+        <div style={{ display: 'flex', overflow: 'auto', backgroundColor: COLORS.surface }}>
+            {tabs}
+        </div>
     </div>;
 };
 
-function getTabStyle(selectedTab: number | null, index: number): React.CSSProperties {
+function menuUi(selectedTab: number | null): JSX.Element {
+    return <div style={{ ...getTabStyle(selectedTab, null), backgroundColor: COLORS.surface }}>
+        Menu
+    </div>;
+}
+
+function getTabStyle(selectedTab: number | null, index: number | null): React.CSSProperties {
     return {
         ...getTabBorderStyle(selectedTab, index),
         padding: '7.5px',
@@ -43,18 +44,21 @@ function getTabStyle(selectedTab: number | null, index: number): React.CSSProper
     };
 }
 
-function getTabBorderStyle(selectedTab: number | null, tabIndex: number): React.CSSProperties {
+function getTabBorderStyle(selectedTab: number | null, tabIndex: number | null): React.CSSProperties {
     const borderRadius = '15px';
     const lightWidth = '2px';
     const strongWidth = '4px';
     const lightBorder = `${lightWidth} solid white`;
     const strongBorder = `${strongWidth} solid ${COLORS.secondary}`;
 
-    if (tabIndex === selectedTab || selectedTab === null) {
+    if (tabIndex === selectedTab || (selectedTab === null && tabIndex === -1)) {
         return {
             borderTop: `${lightWidth} solid ${COLORS.secondary}`,
-            background: `linear-gradient(0deg, ${COLORS.surface}, ${COLORS.primary})`
+            background: `linear-gradient(0deg, ${COLORS.surface}, ${COLORS.primary})`,
+            borderLeft: tabIndex === 0 ? strongBorder : undefined
         };
+    } else if (selectedTab === null) {
+        // return {};
     } else if (tabIndex === selectedTab - 1) {
         return {
             borderTop: lightBorder,
@@ -77,7 +81,7 @@ function getTabBorderStyle(selectedTab: number | null, tabIndex: number): React.
 
     return {
         borderTop: lightBorder,
-        borderRight: lightBorder,
+        borderRight: tabIndex === null && selectedTab === 0 ? undefined : lightBorder,
         borderBottom: strongBorder,
         borderLeft: lightBorder
     };
