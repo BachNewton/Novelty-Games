ae462da577a51a5de2b4154dfa4d04e5155decc7
Kyle Hutchinson <kyle1235@gmail.com>
2025-09-28 14:43:52 -0400

    Overview calc moved to logic

---
+++ src/mobile-games/pets/logic/OverviewData.ts
@@ -0,0 +1,120 @@
+import { Pet } from "../data/Pet";
+import { PET_DATA } from "../data/PetData";
+import { MAX_HEARTS } from "../ui/FriendshipBar";
+
+export interface OverviewData {
+    breakdowns: PetBreakdown[];
+    summary: Summary;
+}
+
+export interface PetBreakdown {
+    isDiscovered: boolean;
+    name: string;
+    maxFriendship: number;
+    friendship: number;
+    isBestFriend: boolean;
+    maxInteractions: number;
+    aquiredInteractions: number;
+}
+
+interface Summary {
+    totalPets: number;
+    discoveredPets: number;
+    totalFriendship: number;
+    maxTotalFriendship: number;
+    bestFriends: number;
+    totalInteractions: number;
+    maxTotalInteractions: number;
+    overallCompletion: number;
+}
+
+export function createOverviewData(
+    pets: Pet[],
+    seenInteractions: Set<string>
+): OverviewData {
+    const totalPets = pets.length;
+
+    let discoveredPets = 0;
+    let totalFriendship = 0;
+    let maxTotalFriendship = 0;
+    let bestFriends = 0;
+    let totalInteractions = 0
+    let maxTotalInteractions = 0;
+
+    const breakdowns = pets.map<PetBreakdown>(pet => {
+        const isDiscovered = pet.discovered;
+        discoveredPets += isDiscovered ? 1 : 0;
+
+        const name = isDiscovered ? pet.name : '???';
+
+        const maxFriendship = MAX_HEARTS;
+        maxTotalFriendship += maxFriendship;
+
+        const friendship = Math.min(pet.friendship, maxFriendship);
+        totalFriendship += friendship;
+
+        const isBestFriend = friendship >= maxFriendship;
+        bestFriends += isBestFriend ? 1 : 0;
+
+        const interactionIds = getInteractionIds(pet);
+
+        const maxInteractions = interactionIds.length;
+        maxTotalInteractions += maxInteractions;
+
+        const aquiredInteractions = countAquiredInteractions(interactionIds, seenInteractions);
+        totalInteractions += aquiredInteractions;
+
+        return {
+            isDiscovered: isDiscovered,
+            name: name,
+            maxFriendship: maxFriendship,
+            friendship: friendship,
+            isBestFriend: isBestFriend,
+            maxInteractions: maxInteractions,
+            aquiredInteractions: aquiredInteractions
+        };
+    });
+
+    const overallCompletion =
+        (discoveredPets + totalFriendship + bestFriends + totalInteractions)
+        /
+        (totalPets + maxTotalFriendship + totalPets + maxTotalInteractions);
+
+
+    return {
+        breakdowns: breakdowns,
+        summary: {
+            overallCompletion: overallCompletion,
+            totalPets: totalPets,
+            discoveredPets: discoveredPets,
+            totalFriendship: totalFriendship,
+            maxTotalFriendship: maxTotalFriendship,
+            bestFriends: bestFriends,
+            totalInteractions: totalInteractions,
+            maxTotalInteractions: maxTotalInteractions
+        }
+    };
+}
+
+function getInteractionIds(pet: Pet): string[] {
+    const data = PET_DATA.find(data => data.id === pet.id);
+
+    if (data === undefined) return [];
+
+    return [
+        data.interactions.pet.id,
+        data.interactions.play.id,
+        data.interactions.space.id,
+        data.interactions.treat.id,
+        ...Array
+            .from(data.interactions.chat)
+            .map(([, interaction]) => interaction.id)
+    ];
+}
+
+function countAquiredInteractions(interactionIds: string[], seenInteractions: Set<string>): number {
+    return interactionIds.reduce(
+        (sum, id) => sum + (seenInteractions.has(id) ? 1 : 0),
+        0
+    ) ?? 0;
+}
--- src/mobile-games/pets/ui/Overview.tsx
+++ src/mobile-games/pets/ui/Overview.tsx
@@ -2,6 +2,7 @@ import VerticalSpacer from "../../../util/ui/Spacer";
 import { TOTAL_FIXED_INTERACTIONS } from "../data/Interaction";
 import { Pet } from "../data/Pet";
 import { PET_DATA } from "../data/PetData";
+import { createOverviewData, PetBreakdown } from "../logic/OverviewData";
 import { MAX_HEARTS } from "./FriendshipBar";
 import { COLORS } from "./Home";
 
@@ -11,48 +12,16 @@ interface OverviewProps {
 }
 
 const Overview: React.FC<OverviewProps> = ({ pets, seenInteractions }) => {
-    const totalPets = pets.length;
-    const discoveredPets = pets.filter(pet => pet.discovered).length;
-    const totalFriendship = MAX_HEARTS * totalPets;
-    const friendship = Math.min(totalFriendship, pets.reduce((sum, pet) => sum + pet.friendship, 0));
-    const totalInteractions = PET_DATA.reduce(
-        (sum, pet) => sum + TOTAL_FIXED_INTERACTIONS + pet.interactions.chat.size,
-        0
-    );
-    const interactions = seenInteractions.size;
+    const { breakdowns, summary } = createOverviewData(pets, seenInteractions);
 
-    const petsFriendship = new Map(pets.map(pet => [pet, Math.min(pet.friendship, MAX_HEARTS)]));
-
-    const petInteractionIds = new Map(pets.map(pet => {
-        const data = PET_DATA.find(data => data.id === pet.id);
-
-        const interationIds = data === undefined ? [] : [
-            data.interactions.pet.id,
-            data.interactions.play.id,
-            data.interactions.space.id,
-            data.interactions.treat.id,
-            ...Array.from(data.interactions.chat.values()).map(interaction => interaction.id)
-        ];
-
-        return [
-            pet,
-            interationIds
-        ]
-    }));
-
-    const petsInteractions = new Map(pets.map(pet => [pet, {
-        aquired: petInteractionIds.get(pet)?.reduce((sum, id) => sum + (seenInteractions.has(id) ? 1 : 0), 0) ?? 0,
-        total: petInteractionIds.get(pet)?.length ?? 0
-    }]));
-
-    const breakdownUi = pets.map((pet, index) => <div key={index} style={{
+    const breakdownUi = breakdowns.map((breakdown, index) => <div key={index} style={{
         border: `1px solid ${COLORS.primary}`,
         borderRadius: '15px',
         marginTop: '15px',
         padding: '10px',
         boxShadow: `0px 0px 10px ${COLORS.secondary}`
     }}>
-        {petBreakdownUi(pet, petsFriendship, petsInteractions)}
+        {petBreakdownUi(breakdown)}
     </div>);
 
     return <>
@@ -69,9 +38,9 @@ const Overview: React.FC<OverviewProps> = ({ pets, seenInteractions }) => {
                 marginBottom: '15px'
             }}>100.00%</div>
 
-            {rowUi('Pets Discovered', discoveredPets, totalPets)}
-            {rowUi('Friendship Gained', friendship, totalFriendship)}
-            {rowUi('Interactinons Seen', interactions, totalInteractions)}
+            {rowUi('Pets Discovered', summary.discoveredPets, summary.totalPets)}
+            {rowUi('Friendship Gained', summary.totalFriendship, summary.totalFriendship)}
+            {rowUi('Interactinons Seen', summary.totalInteractions, summary.maxTotalInteractions)}
         </div>
 
         <VerticalSpacer height={15} />
@@ -88,25 +57,22 @@ function rowUi(label: string, acquired: number, total: number): JSX.Element {
     </>;
 }
 
-function petBreakdownUi(pet: Pet, petsFriendship: Map<Pet, number>, petsInteractions: Map<Pet, { aquired: number; total: number; }>): JSX.Element {
-    const friendship = petsFriendship.get(pet) ?? 0;
-    const interactions = petsInteractions.get(pet) ?? { aquired: 0, total: 0 };
-
+function petBreakdownUi(breakdown: PetBreakdown): JSX.Element {
     return <>
         <div style={{
             fontWeight: 'bold',
             color: COLORS.secondary,
             fontSize: '1.2em',
             marginBottom: '15px'
-        }}>{pet.discovered ? pet.name : '???'}</div>
+        }}>{breakdown.name}</div>
 
         <div style={{
             display: 'grid',
             gridTemplateColumns: 'repeat(4, 1fr)',
             paddingLeft: '30px'
         }}>
-            {rowUi('Friendship', friendship, MAX_HEARTS)}
-            {rowUi('Interactions', interactions.aquired, interactions.total)}
+            {rowUi('Friendship', breakdown.friendship, breakdown.maxFriendship)}
+            {rowUi('Interactions', breakdown.aquiredInteractions, breakdown.maxInteractions)}
         </div>
     </>;
 }
